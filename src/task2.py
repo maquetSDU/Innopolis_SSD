6c97d486940215f2b1f8bc8abb297105fb977fd5
maquetSDU <43057203+maquetSDU@users.noreply.github.com>
2021-09-12 23:35:57 +0300

    Update task2.py

--- src/task2.py
+++ src/task2.py
@@ -11,12 +11,12 @@ def decorator(func):
         and function output
     param func: function from main.py
     '''
-    calls=0
+    calls = 0
 
     def task(*args):
         nonlocal calls
-        calls+=1
-        start=timer()
+        calls += 1
+        start = timer()
         # we temporarily put main.py functions outputs to f
         with redirect_stdout(io.StringIO()) as f:
             out = func(*args)
@@ -25,13 +25,13 @@ def decorator(func):
         s = f.getvalue()
         args = locals()
         # print all info...
-        print(func.__name__ +' call '+ str(calls) + ' executed in '+ str(end-start) + ' sec')
-        print('Name: '+func.__name__)
-        print('Type: '+str(type(func)))
-        print('Sign: '+str(inspect.signature(func)))
+        print(func.__name__ + ' call ' + str(calls) + ' executed in ' + str(end-start) + ' sec')
+        print('Name: ' + func.__name__)
+        print('Type: ' + str(type(func)))
+        print('Sign: ' + str(inspect.signature(func)))
         print('Args: ' + str(args['args']))
-        print('Doc: '+str(func.__doc__))
-        print('Source: '+str(inspect.getsource(func)))
+        print('Doc: '+ str(func.__doc__))
+        print('Source: ' + str(inspect.getsource(func)))
         print('Outputs: ', s)
         print('function outputs: ', out,"\n")
     return task
